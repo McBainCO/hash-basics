ebb0ea86aca910d9eaa47794954d2d49ccdf3c7b
Graham McBaingrahammcbain@gmail.comgrahammcbain@gmail.comgit config --global user.email your_email@example.comgit config --global user.name Graham <grahammcbain@gmail.comgit config --global user.emailgrahammcbain@gmail.comgit config --global user.email grahammcbain@gmail.com>
2014-06-19 14:07:07 -0600

    printed address properly

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,13 @@
 require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
+  BJORN_BORG["address"]["street"] = "444 Borg Lane, "
+  BJORN_BORG["address"]["city"] = "San Francisco, "
+  BJORN_BORG["address"]["state"] = "CA, "
+
+
+
+ print BJORN_BORG["address"]["street"]
+ print BJORN_BORG["address"]["city"]
+ print BJORN_BORG["address"]["state"]
+ puts BJORN_BORG["address"]["zip_code"]
